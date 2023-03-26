5e73285f680f3d61cf299ae08093ddebe796994a
Bibek Acharya <acharya.bbk@gmail.com>
2023-03-26 22:04:22 +0000

    firstr commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -28,14 +29,19 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let name = `<h1>${variables.name}</h1>`;
+  let lname = `<h1>${variables.lastname}</h1>`;
+  let role = `<h2>${variables.role}</h2>`;
+  let country = `<h3>${variables.country}</h>`;
+  let city = `<h3>${variables.city}</h3>`;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${name + " " + lname}</h1>
+          <h2>${role}</h2>
+          <h3>${city + ", " + country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
